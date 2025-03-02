82789a82819bae330a66438f1db3f51dc36c904d
jeremydickens <jeremy@offnominal.com>
2025-03-02 07:03:32 -0600

    Command handling

--- index.js
+++ index.js
@@ -44,3 +44,26 @@ client.once(Events.ClientReady, readyClient => {
 
 // Log in to Discord with your client's token
 client.login(token);
+
+client.on(Events.InteractionCreate, async interaction => {
+    if(!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`No Command matching ${interaction.commandName} found.`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({ content: 'There was an error while executing this command.', flags: MessageFlags.Ephemeral });
+            ;
+        } else {
+            await interaction.reply({ content: 'There was an error while executing this command.', flags: MessageFlags.Ephemeral });
+        }
+    }
+});
